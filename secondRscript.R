c096ce3e9f05168c2f5529920ee93c6ff95fd1f4
Danlu0217 <52614247+Danlu0217@users.noreply.github.com>
2019-08-26 09:32:59 -0400

    Update secondRscript.R
    Name the activities and get the  means

--- secondRscript.R
+++ secondRscript.R
@@ -7,24 +7,62 @@ d<-read.table("train/Y_train.txt")
 newData<-rbind(a,c)
 #Get where/in which column "Mean()" functions are located from features file
 features<-read.table("features.txt")
-#serach for functions
-meanV<-grep("mean",features$V2)
+#serach for functions,excluding "meanFreq"
+meanV<-grep("\\<mean\\>",features$V2)
 #Get the name for the activity
-NameMean<-features$V2[meanV]
-#Convert name factors to characters
+NameMean<-features$V2[grep("\\<mean\\>",features$V2)]
+#Get all the data with mean functions from the new merged files
+mean_tot<-newData[,meanV]
 NameMean1<-as.character(NameMean)
 #Get all the data with mean functions from the new merged files
 mean_tot<-newData[,meanV]
 #Get std columns
 stdV<-grep("std",features$V2)
-#Get the activity names
+#Get the activity namesNamestd<-features$V2[stdV]
 Namestd<-features$V2[stdV]
 Namestd1<-as.character(Namestd)
+#Make a label vector
+label<-c(NameMean1,Namestd1)
+#Get merged activity number labels
+ActNum<-rbind(b,d)
+actnum<-ActNum[,1]
 #Get all the data with std functions from the new merged files
 std_tot<-newData[,stdV]
 #Final merged file with means and standard deviations
 newtable1<-cbind(mean_tot,std_tot)
+names(newtable1)<-label
+#Make actviity names
+newn1<-gsub("1","WALKING",actnum)
+newn2<-gsub("2","WALKING_UPSTAIRS",newn1)
+newn3<-gsub("3","WALKING_DOWNSTAIRS",newn2)
+newn4<-gsub("4","SITTING",newn3)
+newn5<-gsub("5","STANDING",newn4)
+newn6<-gsub("6","LAYING",newn5)
+actname<-as.factor(newn6)
+#Put  activity name in the new table as a column
+library(dplyr)
+finaltable1<-mutate(newtable1,actname)
 newM<-colMeans(newtable1)
-nametot<-c(NameMean1,Namestd1)
-newtable2<-data.frame(cbind(newM,nametot))
+data1<-finaltable1[finaltable1$actname=="WALKING",]
+data2<-finaltable1[finaltable1$actname=="WALKING_UPSTAIRS",]
+data3<-finaltable1[finaltable1$actname=="WALKING_DOWNSTAIRS",]
+data4<-finaltable1[finaltable1$actname=="SITTING",]
+data5<-finaltable1[finaltable1$actname=="STANDING",]
+data6<-finaltable1[finaltable1$actname=="LAYING",]
+#mean for WALKING for each measurement
+d1mean<-colMeans(data1[,1:66])
+#Mean for WALKING_UPSTAIRS
+d2mean<-colMeans(data2[,1:66])
+#Mean for WALKING_DOWNSTAIRS
+d3mean<-colMeans(data3[,1:66])
+#Mean for SITTING
+d4mean<-colMeans(data4[,1:66])
+#Mean for STANDING
+d5mean<-colMeans(data5[,1:66])
+#Mean for LAYING
+d6mean<-colMeans(data6[,1:66])
+meandata<-data.frame(d1mean,d2mean,d3mean,d4mean,d5mean,d6mean)
+act<-c("WALKING","WALKING_UPSTAIRS","#WALKING_DOWNSTAIRS","SITING","STANDING","LAYING")
+#Rename the columns
+names(meandata)<-act
 
